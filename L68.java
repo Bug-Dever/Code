cd9eefd62954e8a6bbcb713b24a91f28f54d7c5d
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-03-18 12:48:38 +0800

    Update L68.java

--- L68.java
+++ L68.java
@@ -7,6 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// 自己写的 辣鸡
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         return recur(root, p, q);
@@ -25,5 +26,34 @@ class Solution {
             return recur(node.right, p, q);
         }
         return null;
+    }
+}
+
+// 递推 时间On 空间O1
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null) {
+            if(p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if(p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+}
+
+// 递归 时间On 空间On
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
     }
 }
