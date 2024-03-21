a42e65b944259925b3cb2cd6ff9566f8b835963f
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-03-21 13:16:57 +0800

    Update 无重复字符的最长字串.java

--- 滑动窗口/无重复字符的最长字串.java
+++ 滑动窗口/无重复字符的最长字串.java
@@ -15,3 +15,18 @@ class Solution {
         return res;
     }
 }
+
+// 用同向双指针的方法做
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int i = 0, res = 0;
+        for(int j = 0; j < s.length(); j++) {
+            while(set.contains(s.charAt(j)))
+                set.remove(s.charAt(i++));
+            set.add(s.charAt(j));
+            res = Math.max(res, j - i + 1);
+        }
+        return res;
+    }
+}
