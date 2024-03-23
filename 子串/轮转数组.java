6a46305fa72872236d2a66a290da68b04d4f90f1
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-03-23 11:54:46 +0800

    Create 轮转数组.java

---
+++ 子串/轮转数组.java
@@ -0,0 +1,18 @@
+class Solution {
+    // 翻转数组
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] arr, int i, int j) {
+        int temp;
+        while(i < j) {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++; j--;
+        }
+    }
+}
