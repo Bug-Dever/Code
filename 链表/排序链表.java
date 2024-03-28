2ee3a2698c6d529d11b94d92c480604a925525f3
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-03-28 12:09:57 +0800

    Create 排序链表.java

---
+++ 链表/排序链表.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        // 使用快慢指针，找到链表中点(偶数找中心左边的)
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // slow 就指向中间了,拆分链表
+        ListNode temp = slow.next;
+        slow.next = null;
+        // 递归，继续拆分
+        ListNode left = sortList(head);
+        ListNode right = sortList(temp);
+        // 合并链表
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+}
