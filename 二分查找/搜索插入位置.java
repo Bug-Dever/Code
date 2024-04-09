4e77f833b0fffb25b17172a10ae4538575de7940
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-04-09 11:09:36 +0800

    Update 搜索插入位置.java

--- 二分查找/搜索插入位置.java
+++ 二分查找/搜索插入位置.java
@@ -1,15 +1,37 @@
+// 无重复元素写法
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        return binarySort(nums, target, 0, nums.length - 1);
-    }
-    public int binarySort(int[] nums, int target, int left, int right) {
-        if(left > right)
-            return left;
-        int mid = left + (right - left) / 2;
-        if(nums[mid] < target) {
-            return binarySort(nums, target, mid + 1, right);
-        } else {
-            return binarySort(nums, target, left, mid - 1);
+        int i = 0, j = nums.length - 1, mid;
+        while(i <= j) {
+            mid = i + (j - i) / 2;
+            if(nums[mid] > target) {
+                j = mid - 1;
+            }
+            else if(nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                return mid;
+            }
         }
+        return i;
+    }
+}
+
+// 若有重复元素，当nums[mid] = target时，j = mid - 1;
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i = 0, j = nums.length - 1, mid;
+        while(i <= j) {
+            mid = i + (j - i) / 2;
+            if(nums[mid] > target) {
+                j = mid - 1;
+            }
+            else if(nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return i;
     }
 }
