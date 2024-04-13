7ee2e229b0fe258bfbc5dc1eb8101297de001be3
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-04-13 17:16:50 +0800

    Update 划分字母区间.java

--- 贪心/划分字母区间.java
+++ 贪心/划分字母区间.java
@@ -1,3 +1,4 @@
+// 自己写的
 class Solution {
     public List<Integer> partitionLabels(String s) {
         Map<Character, Integer> map =new HashMap<>(); // map用来存每个字符最后的位置
@@ -15,6 +16,27 @@ class Solution {
                     right = map.get(arr[j]);
             }
             ans.add(right - i + 1);
+        }
+        return ans;
+    }
+}
+
+// 优化,更好的代码
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26]; // 用一个数组代替map来存储字符最后一次出现的位置
+        List<Integer> ans = new ArrayList<>();
+        char[] arr = s.toCharArray();
+        for(int m = 0; m < arr.length; m++) {
+            last[arr[m] - 'a'] = m;
+        }
+        int start = 0, end = 0;
+        for(int i = 0; i < arr.length; i++) {
+            end = Math.max(end, last[arr[i] - 'a']);
+            if(i == end) {
+                ans.add(end - start + 1);
+                start = end + 1;
+            }
         }
         return ans;
     }
