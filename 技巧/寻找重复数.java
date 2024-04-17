2e5fde6763599e20899260fdd561d7fdad5b4d7d
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-04-17 10:36:21 +0800

    Update 寻找重复数.java

--- 技巧/寻找重复数.java
+++ 技巧/寻找重复数.java
@@ -1,3 +1,4 @@
+// 法一：
 class Solution {
     public int findDuplicate(int[] nums) {
         // 原地哈希
@@ -12,5 +13,26 @@ class Solution {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
+    }
+}
+
+// 法二：
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            if(i == nums[i]) {
+                i++;
+                continue;
+            }
+            if(nums[i] == nums[nums[i]]) {
+                return nums[i];
+            }
+
+            int tmp = nums[i];
+            nums[i] = nums[nums[i]];
+            nums[tmp] = tmp;
+        }
+        return -1;
     }
 }
