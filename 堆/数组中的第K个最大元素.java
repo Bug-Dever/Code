a14c190fa39bfc7eb01353631e0bef3bc96a857b
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-04-18 20:56:28 +0800

    Update 数组中的第K个最大元素.java

--- 堆/数组中的第K个最大元素.java
+++ 堆/数组中的第K个最大元素.java
@@ -1,3 +1,4 @@
+// 小顶堆 -> 优先队列实现
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Queue<Integer> minHeap = new PriorityQueue<>();
@@ -15,5 +16,46 @@ class Solution {
             }
         }
         return minHeap.peek();
+    }
+}
+
+// 快排
+class Solution {
+    private final static Random random = new Random(System.currentTimeMillis());
+    public int findKthLargest(int[] nums, int k) {
+        int target = nums.length - k; // 第 k 个最大的元素
+
+        int left = 0, right = nums.length - 1;
+        while(true) {
+            int pivot = partition(nums, left, right);
+            if(pivot < target) left = pivot + 1;
+            else if(pivot > target) right = pivot - 1;
+            else return nums[pivot];
+        }
+
+    }
+    public int partition(int[] nums, int left, int right) {
+        // 不交换的话会超时
+        int randomIndex = left + random.nextInt(right - left + 1);
+        int temp = nums[randomIndex];
+        nums[randomIndex] = nums[left];
+        nums[left] = temp;
+
+
+        int i = left, j = right;
+        while(i < j) {
+            while(i < j && nums[j] >= nums[left])
+                j--;
+            while(i < j && nums[i] <= nums[left])
+                i++;
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+        int tmp = nums[i];
+        nums[i] = nums[left];
+        nums[left] = tmp;
+
+        return i;
     }
 }
