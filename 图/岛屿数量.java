e8e243ddab01cae16e474aea2d5246f4df392fdd
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-04-26 10:43:32 +0800

    Update 岛屿数量.java

--- 图/岛屿数量.java
+++ 图/岛屿数量.java
@@ -1,3 +1,4 @@
+// dfs
 class Solution {
     public int numIslands(char[][] grid) {
         int ans = 0;
@@ -19,5 +20,37 @@ class Solution {
         dfs(grid, i + 1, j);
         dfs(grid, i, j - 1);
         dfs(grid, i, j + 1);
+    }
+}
+
+// bfs
+class Solution {
+    Queue<int []> queue = new LinkedList<>();
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1') {
+                    ans++;
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+    public void bfs(char[][] grid, int i, int j) {
+        queue.offer(new int[]{i, j});
+        while(!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            i = arr[0]; j = arr[1];
+            if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+                continue;
+            grid[i][j] = '0';
+            queue.offer(new int[]{i - 1, j});
+            queue.offer(new int[]{i + 1, j});
+            queue.offer(new int[]{i, j - 1});
+            queue.offer(new int[]{i, j + 1});           
+        }
+        
     }
 }
