8bb1572ad87c6f2ff8076a95f3c9981fca5946af
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-04-28 22:17:54 +0800

    Create 17电话号码的字母组合.java

---
+++ 回溯/17电话号码的字母组合.java
@@ -0,0 +1,25 @@
+class Solution {
+    int n;
+    List<String> ans = new ArrayList<>();
+    char[] path, digits;
+    String[] MAPPING = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    public List<String> letterCombinations(String digits) {
+        n = digits.length();
+        if(n == 0)
+            return new ArrayList<String>();
+        path = new char[n];
+        this.digits = digits.toCharArray();
+        dfs(0);
+        return ans;
+    }
+    public void dfs(int i) {
+        if(i == n) {
+            ans.add(new String(path));
+            return;
+        }
+        for(char c : MAPPING[digits[i] - '0'].toCharArray()) {
+            path[i] = c;
+            dfs(i + 1);
+        }
+    }
+}
