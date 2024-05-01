ac74479e5c802487c067b620182c1f92113d574e
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-05-01 13:24:32 +0800

    Create 78子集.java

---
+++ 回溯/78子集.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    int n;
+    int[] nums;
+    public List<List<Integer>> subsets(int[] nums) {
+        this.n = nums.length;
+        this.nums = nums;
+        dfs(0);
+        return ans;
+    }
+    public void dfs(int i) {
+        if(i == n) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        // 不选
+        dfs(i + 1);
+        // 选
+        path.add(nums[i]);
+        dfs(i + 1);
+        // 恢复现场
+        path.remove(path.size() - 1);
+    }
+}
