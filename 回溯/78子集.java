665abb41184cafd49a854ad54d43f1864cfbeee3
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-05-03 22:17:35 +0800

    Update 78子集.java

--- 回溯/78子集.java
+++ 回溯/78子集.java
@@ -1,4 +1,4 @@
-// 
+// 法一：
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     List<Integer> path = new ArrayList<>();
@@ -22,5 +22,32 @@ class Solution {
         dfs(i + 1);
         // 恢复现场
         path.remove(path.size() - 1);
+    }
+}
+
+// 法二：
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    int n;
+    int[] nums;
+    public List<List<Integer>> subsets(int[] nums) {
+        this.n = nums.length;
+        this.nums = nums;
+        dfs(0);
+        return ans;
+    }
+    public void dfs(int i) {
+        ans.add(new ArrayList<>(path));
+        if(i == n) {
+            return;
+        }
+        for(int j = i; j < n; j++) {
+            path.add(nums[j]);
+            dfs(j + 1);
+            // 恢复现场
+            path.remove(path.size() - 1);
+        }
+
     }
 }
