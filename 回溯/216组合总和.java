4ef2609d537e113f0240a4d3c8b759b598fd0276
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-05-06 11:43:15 +0800

    Create 216组合总和.java

---
+++ 回溯/216组合总和.java
@@ -0,0 +1,33 @@
+// 法二：倒序枚举，剪枝
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    int k;
+    int n;
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        this.k = k;
+        this.n = n;
+        dfs(9);
+        return ans;
+    }
+    public void dfs(int i) {
+        // 剪枝
+        int d = k - path.size();
+        if(i < d || n < 0 || n > (i + i - d + 1) * d / 2)
+            return;
+        
+        if(path.size() == k && n == 0) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int j = i; j > 0; j--) {
+            path.add(j);
+            n -= j;
+            dfs(j - 1);
+            // 恢复现场
+            n += j;
+            path.remove(path.size() - 1);
+        }
+    }
+}
