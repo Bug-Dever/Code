b5f5e4aafd7787d57d8c43e42e7e1a815dc60961
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-05-18 10:27:09 +0800

    Create 19删除链表的倒数第N个节点.java

---
+++ 我很好也很想你/链表/19删除链表的倒数第N个节点.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode slow = dummy, fast = dummy;
+        // 快指针先走 n 步
+        while(n-- > 0) {
+            fast = fast.next;
+        }
+        while(fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
