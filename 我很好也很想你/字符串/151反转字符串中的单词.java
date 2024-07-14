9ed6987541b619146cc05787309307b57cd957d1
Gush <98139182+Bug-Dever@users.noreply.github.com>
2024-07-14 22:04:42 +0800

    Update 151反转字符串中的单词.java

--- 我很好也很想你/字符串/151反转字符串中的单词.java
+++ 我很好也很想你/字符串/151反转字符串中的单词.java
@@ -1,13 +1,13 @@
 // 双指针，从末尾向前
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
-        int i = s.length() - 1, j = i;
+        s = s.trim(); // 去掉首尾空格
+        int i = s.length() - 1, j = i; // 规定 i 在左边，j 在右边
         StringBuilder sb = new StringBuilder();
         while(i >= 0) {
             while(i >= 0 && s.charAt(i) != ' ') i--;
-            sb.append(s.substring(i + 1, j + 1) + " ");
-            
+            sb.append(s.substring(i + 1, j + 1));
+            sb.append(" "); // 添加单词间空格
             while(i >= 0 && s.charAt(i) == ' ') i--;
             j = i;
         }
